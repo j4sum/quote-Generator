001f49e698c7a7db306b59af3839552e9377a64c
Sumeyya Javaid <jasum@protonmail.com>
2022-08-27 23:31:40 +0100

    add .active class dynamically to show spinner

--- src/components/quote-generator/quotes/Quotes.js
+++ src/components/quote-generator/quotes/Quotes.js
@@ -5,87 +5,91 @@ import { Link } from "react-router-dom";
 import Loader from "../../Loader";
 
 const Quotes = () => {
-  // getting Parameters from Url
-  const { quotes: author } = useParams();
+    // getting Parameters from Url
+    const { quotes: author } = useParams();
 
-  const [quotes, setQuotes] = useState([]);
-  const [randAuthor, setRandAuthor] = useState("");
-  const [loading, setLoading] = useState(false);
-  const FETCH_URL = `https://quote-garden.herokuapp.com/api/v3/quotes/?author=${author}`;
-  const FETCH_AUTHORS_URL = `https://quote-garden.herokuapp.com/api/v3/authors`;
+    const [quotes, setQuotes] = useState([]);
+    const [randAuthor, setRandAuthor] = useState("");
+    const [loading, setLoading] = useState(false);
+    const [isActive, setIsActive] = useState(false);
+    const FETCH_URL = `https://quote-garden.herokuapp.com/api/v3/quotes/?author=${author}`;
+    const FETCH_AUTHORS_URL = `https://quote-garden.herokuapp.com/api/v3/authors`;
 
-  // fetching data on first mount
- 
+    // fetching data on first mount
 
-  useEffect(() => {
-    async function fetchQuotes() {
-      setLoading(true);
-      try {
-        await axios.get(FETCH_URL).then((res) => {
-          setQuotes(res.data.data);
-        });
-        setLoading(false);
-      } catch (error) {
-        console.log(error.message);
-      }
-      const request = await axios.get(FETCH_URL);
-      setQuotes(request.data.data);
+
+    useEffect(() => {
+        async function fetchQuotes() {
+            setLoading(true);
+            try {
+                await axios.get(FETCH_URL).then((res) => {
+                    setQuotes(res.data.data);
+                });
+                setLoading(false);
+            } catch (error) {
+                console.log(error.message);
+            }
+            const request = await axios.get(FETCH_URL);
+            setQuotes(request.data.data);
+        }
+
+        fetchQuotes();
+    }, [FETCH_URL]);
+
+    useEffect(() => {
+        async function fetchAllAuthors() {
+            const request = await axios.get(FETCH_AUTHORS_URL);
+            const randomNo = Math.floor(Math.random() * request.data.data.length);
+            setRandAuthor(request.data.data[randomNo]);
+        }
+
+        fetchAllAuthors();
+    }, [FETCH_AUTHORS_URL, randAuthor]);
+
+    const toggleActiveClass = () => {
+        setIsActive((prev) => !prev)
     }
-    
-    fetchQuotes();
-  }, [FETCH_URL]);
 
-  useEffect(() => {
-    async function fetchAllAuthors() {
-      const request = await axios.get(FETCH_AUTHORS_URL);
-      const randomNo = Math.floor(Math.random() * request.data.data.length);
-      setRandAuthor(request.data.data[randomNo]);
-    }
-
-    fetchAllAuthors();
-  }, [FETCH_AUTHORS_URL, randAuthor]);
-
-  return (
-    <div className="container mt-2 pl-9">
-      {/* fetch random authors Data */}
-      <Link
-        to={`/${randAuthor}`}
-        className="justify-end pr-5 flex items-center cursor-pointer"
-      >
-        <span className="font-medium text-lg">random</span>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          className="w-4 ml-1"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
-          />
-        </svg>
-      </Link>
-
-      {!loading ? (
-        <div className="w-6/7 md:w-5/7 mx-auto m-sm">
-          <h1 className="font-bold text-2xl"> {author} </h1>
-          {quotes.map((quote, index) => (
-            <p
-              className="font-medium text-lg md:text-xl mt-9 relative quote pl-8"
-              key={index}
+    return ( <
+            div className = "container mt-2 pl-9" > { /* fetch random authors Data */ } <
+            Link to = { `/${randAuthor}` }
+            className = { `randAuthor${isActive ? :''}` }
+            onClick = { toggleActiveClass }
+            className = "justify-end pr-5 flex items-center cursor-pointer" >
+            <
+            span className = "font-medium text-lg" > random < /span> <
+            svg xmlns = "http://www.w3.org/2000/svg"
+            fill = "none"
+            viewBox = "0 0 24 24"
+            stroke = "currentColor"
+            className = "w-4 ml-1" >
+            <
+            path strokeLinecap = "round"
+            strokeLinejoin = "round"
+            strokeWidth = "2"
+            d = "M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" /
             >
-              {quote.quoteText}
-            </p>
-          ))}
-        </div>
-      ) : (
-        <Loader />
-      )}
-    </div>
-  );
+            <
+            /svg> < /
+            Link >
+
+            {!loading ? ( <
+                    div className = "w-6/7 md:w-5/7 mx-auto m-sm" >
+                    <
+                    h1 className = "font-bold text-2xl" > { author } < /h1> {
+                    quotes.map((quote, index) => ( <
+                        p className = "font-medium text-lg md:text-xl mt-9 relative quote pl-8"
+                        key = { index } > { quote.quoteText } <
+                        /p>
+                    ))
+                } <
+                /div>
+            ): ( <
+                Loader / >
+            )
+        } <
+        /div>
+);
 };
 
 export default Quotes;
